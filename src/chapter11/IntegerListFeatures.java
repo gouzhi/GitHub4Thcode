fdd819be00d06cbaa229386ab7a970608234d359
gouzhi <381775433@qq.com>
2016-08-02 22:15:12 +0800

    test

--- src/chapter11/IntegerListFeatures.java
+++ src/chapter11/IntegerListFeatures.java
@@ -13,55 +13,53 @@ import chapter14.Pet;
 import chapter14.Pets;
 import static com.maoruxin.javabook.util.Print.*;
 
-public class ListFeatures {
+public class IntegerListFeatures {
 	public static void main(String[] args) {
 		Random rand = new Random(47);
-		List<Pet> pets = Pets.arrayList(7);
+		List<Integer> pets = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6));
 		print("1: " + pets);
-		Hamster h = new Hamster();
-		pets.add(h); 	//  Automatically resizes
+		pets.add(3); 	//  Automatically resizes
 		print("2 " + pets);
-		print("3 " + pets.contains(h));
-		pets.remove(h);
-		Pet p = pets.get(2);
+		print("3 " + pets.contains(3));
+		Integer t = 3;
+		pets.remove(t);
 		print("3 remove " + pets);
+		Integer p = pets.get(2);
 		print("4 " + p + " " + pets.indexOf(p) );
-		Pet cymric = new Cymric();
+		Integer cymric = 3;
 		print("5 " + pets.indexOf(cymric));
 		print("6 " + pets.remove(cymric));
+		print("6 remove " + pets);
 		// Must be the exact object
 		print("7 " + pets.remove(p));
 		print("8 " + pets);
-		pets.add(3, new Mouse()); // Insert at an index
+		pets.add(3, 8); // Insert at an index
 		print("9 " + pets);
-		List<Pet> sub = pets.subList(1, 4);
+		List<Integer> sub = pets.subList(1, 4);
 		print("sublist " + sub);
-		Collections.sort(pets);
-		print("pets" + pets);
-		print("subList: " + sub);
 		print("10 " + pets.containsAll(sub));
 		Collections.sort(sub); // In-place sort
 		print("sorted subList " + sub);
-		// Order is not import in containsAll();
+//		// Order is not import in containsAll();
 		print("11 " + pets.containsAll(sub));
 		print("pets" + pets);
 		Collections.shuffle(sub, rand); // Mix it up
 		print("shuffled subList: " + sub);
 		print("pets" + pets);
 		print("12 " + pets.containsAll(sub));
-		List<Pet> copy = new ArrayList<Pet>(pets);
+		List<Integer> copy = new ArrayList<Integer>(pets);
 		sub = Arrays.asList(pets.get(1),pets.get(4));
 		print("pets" + pets);
 		print("sub " + sub);
 		copy.retainAll(sub);
 		print("13 " + copy);
-		copy = new ArrayList<Pet>(pets);
+		copy = new ArrayList<Integer>(pets);
 		print("fresh copy " + copy);
 		copy.remove(2);
 		print("14 " + copy);
 		copy.removeAll(sub);
 		print("15 " + copy);
-		copy.set(1, new Mouse()); // Replace an element
+		copy.set(1, 9); // Replace an element
 		print("16 " + copy);
 		copy.addAll(2, sub);
 		print("17 " + copy);
@@ -71,12 +69,10 @@ public class ListFeatures {
 		print("20 " + pets.isEmpty());
 		pets.add(null);
 		print("20 null " + pets.isEmpty());
-		pets.addAll(Pets.arrayList(4));
+		pets.addAll(Arrays.asList(10,11,12,13,14,15));
 		print("21 " + pets);
 		Object[] o = pets.toArray();
 		print("22 " + o[3]);
-		Pet[] pa = pets.toArray(new Pet[0]);
-		print("23 " + pa[3].id());
 		
 	}
 }
